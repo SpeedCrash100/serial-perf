10f6a1710388657ff5cd6d2b44235ff3eea269a6
Anton Kushakov <SpeedCrash100@yandex.ru>
2024-09-30 10:49:48 +0300

    chore(example): Added loopback example

---
+++ serial-perf/examples/linux_loopback.rs
@@ -0,0 +1,30 @@
+use clap::Parser;
+use linux_embedded_hal::Serial;
+use serial_perf::loopback::Loopback;
+
+#[derive(Parser)]
+pub struct CommonArgs {
+    /// The port to connect to.
+    port: String,
+
+    #[clap(short, long, default_value_t = 115200)]
+    baud_rate: u32,
+}
+
+impl CommonArgs {
+    pub fn create_serial(&self) -> Serial {
+        Serial::open(self.port.clone(), self.baud_rate).expect("failed to create serial")
+    }
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = CommonArgs::parse();
+
+    let serial = args.create_serial();
+    let mut loopback = Loopback::new(serial);
+
+    println!("Start loop");
+    loop {
+        nb::block!(loopback.loop_nb())?;
+    }
+}
